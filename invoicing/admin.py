77e77f662f82ca9307f54e2b94918d8c1984846d
Shaheer Hasan <hshaheer99@gmail.com>
2024-11-16 20:27:27 +0530

    Add admin config for models

--- invoicing/admin.py
+++ invoicing/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Item, Purchase, PurchaseItem
 
-# Register your models here.
+# Admin interface configuration for the Item model
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'description')
+    
+    # Enable search functionality by item name
+    search_fields = ('name',)
+
+# Admin interface configuration for the Purchase model
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at')
+    
+    # Add a filter for the 'created_at' field in the list view
+    list_filter = ('created_at',)
+
+# Admin interface configuration for the PurchaseItem model
+class PurchaseItemAdmin(admin.ModelAdmin):
+    list_display = ('purchase', 'item', 'quantity')
+    
+    # Enable search functionality by item name within PurchaseItem
+    search_fields = ('item__name',)
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
+admin.site.register(PurchaseItem, PurchaseItemAdmin)
