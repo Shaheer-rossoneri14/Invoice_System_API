a9d5b6804a367ad20e47d47b0e852fdec26eeda0
Shaheer Hasan <hshaheer99@gmail.com>
2024-11-16 20:24:36 +0530

    Add Purchase and PurchaseItem models

--- invoicing/models.py
+++ invoicing/models.py
@@ -8,3 +8,13 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+class Purchase(models.Model):
+    # Link items with purchases via a through model (PurchaseItem)
+    items = models.ManyToManyField(Item, through='PurchaseItem')
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+class PurchaseItem(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
