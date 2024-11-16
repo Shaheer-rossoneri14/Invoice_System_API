f8b1876d5a13a228888fb43ecbc8a10054f16f81
Shaheer Hasan <hshaheer99@gmail.com>
2024-11-16 20:26:55 +0530

    Add purchase and purchase<id> endpoints

--- invoicing/urls.py
+++ invoicing/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ItemListView
+from .views import CreatePurchaseView, ItemListView, UpdatePurchaseView
 
 urlpatterns = [
     path('items/', ItemListView.as_view(), name='item-list'),
+    path('purchase/', CreatePurchaseView.as_view(), name='create-purchase'),
+    path('purchase/<int:id>/', UpdatePurchaseView.as_view(), name='update-purchase'),
 ]
