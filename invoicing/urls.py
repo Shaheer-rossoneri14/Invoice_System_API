032e592fc2981c1437f6a7a24c40624e40e2a89d
Shaheer Hasan <hshaheer99@gmail.com>
2024-11-21 12:39:50 +0530

    Updated name in path to fix unit test errors due to reverse function

--- invoicing/urls.py
+++ invoicing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreatePurchaseView, ItemListView, UpdatePurchaseView
+from .views import CreatePurchaseView, ItemListView, UpdatePurchaseView, InvoiceView
 
 urlpatterns = [
     path('items/', ItemListView.as_view(), name='item-list'),
     path('purchase/', CreatePurchaseView.as_view(), name='create-purchase'),
     path('purchase/<int:id>/', UpdatePurchaseView.as_view(), name='update-purchase'),
+    path('invoice/<int:id>/', InvoiceView.as_view(), name='generate-invoice'),
 ]
